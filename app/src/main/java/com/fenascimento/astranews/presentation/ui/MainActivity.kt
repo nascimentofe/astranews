294b960a6c9a66c7a2869accfb83f508ff9e42b4
Felipe do Nascimento <felipeleforte@outlook.com>
2022-07-13 00:28:55 -0300

    Projeto iniciado.

---
+++ app/src/main/java/com/fenascimento/astranews/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fenascimento.astranews.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fenascimento.astranews.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
